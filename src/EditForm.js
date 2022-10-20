ec1292b502590e95930d43d2cd26f7f7a65e9050
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-10-20 08:33:04 -0400

    polishing styles

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import useInputState from './hooks/useInputState';
-import './styles/TodoForm.css'
+import React from "react";
+import useInputState from "./hooks/useInputState";
+import "./styles/TodoForm.css";
 
-function EditForm({id, task, editToggle, editTodo}) {
-  const [value, handleChange, reset] = useInputState(task)
+function EditForm({ id, task, editToggle, editTodo }) {
+	const [value, handleChange] = useInputState(task);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    editTodo(id, value);
-    editToggle();
-  }
-  return (
-      <form onSubmit={handleSubmit} className='EditForm'>
-      <input
-       type="text"
-       value={value}
-       onChange={handleChange}
-       className="FormInput"
-       style={{textAlign: 'left'}}
-      />
-      {/*   <i type="submit" className="fa-solid fa-check-double" /> */}
-    </form>
-  )
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		editTodo(id, value);
+		editToggle();
+	};
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="EditForm"
+		>
+			<input
+				type="text"
+				value={value}
+				onChange={handleChange}
+				className="FormInput"
+				style={{ textAlign: "left" }}
+			/>
+			{/*   <i type="submit" className="fa-solid fa-check-double" /> */}
+		</form>
+	);
 }
 
-export default EditForm
+export default EditForm;
